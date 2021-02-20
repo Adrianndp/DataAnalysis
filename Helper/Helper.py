3e77e155804825553e85363a86717c4a9ba6637e
Adrian <adrianndp@hotmail.com>
2021-02-20 20:46:01 +0100

    organized

--- Helper/Helper.py
+++ Helper/Helper.py
@@ -8,3 +8,10 @@ def sort_dict_by_key(dict_input):
     return json.dumps(dict_output)
 
 
+def get_risk_from_market_cap(market_cap):
+    # 10 billion
+    if market_cap > 10000000000:
+        return "low"
+    if market_cap > 3000000000:
+        return "middle"
+    return "high"
